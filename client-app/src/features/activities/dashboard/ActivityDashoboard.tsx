a45cbc4ecc55bf7e9f825d927fea2f7e3bbb17a9
Paderich <paderich@DESKTOP-99QPLBT>
2023-03-04 10:02:51 +0100

    end of section 6

--- client-app/src/features/activities/dashboard/ActivityDashoboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashoboard.tsx
@@ -14,6 +14,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard(
@@ -26,7 +27,8 @@ export default function ActivityDashboard(
         openForm,
         closeForm,
         createOrEdit,
-        deleteActivity
+        deleteActivity,
+        submitting
     }: Props) {
     return (
         <Grid>
@@ -35,6 +37,7 @@ export default function ActivityDashboard(
                     activities={activities}
                     selectActivity={selectActivity}
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width={6}>
@@ -46,7 +49,11 @@ export default function ActivityDashboard(
 
                     />}
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                    <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit} 
+                    submitting={submitting}/>}
             </Grid.Column>
         </Grid>
     )
